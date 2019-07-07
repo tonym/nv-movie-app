539f5256317dadc174cd315dc76b1849d70b3cd0
Tony M <tony@inspecdigital.org>
2019-07-07 15:14:49 -0700

    add auth tests

---
+++ src/store/auth/auth.test.ts
@@ -0,0 +1,45 @@
+import { action } from 'typesafe-actions';
+import { login, logout } from './authActions';
+import { authReducer } from './authReducer';
+import { AuthActionTypes } from './authTypes';
+
+describe('Auth actions', () => {
+
+  it('should create an action to login a user', () => {
+    const user = 'Orson Welles'
+    const expectedAction = action(AuthActionTypes.LOGIN, user);
+    expect(login(user)).toEqual(expectedAction)
+  })
+
+  it('should create an action to logout a user', () => {
+    const expectedAction = action(AuthActionTypes.LOGOUT);
+    expect(logout()).toEqual(expectedAction)
+  })
+
+});
+
+describe('Auth reducer', () => {
+
+  const user = 'Orson Welles';
+  const loginAction = action(AuthActionTypes.LOGIN, user);
+  const logoutAction = action(AuthActionTypes.LOGOUT);
+
+  it('should return the initial state', () => {
+    expect(authReducer(undefined, {})).toEqual({
+      user: '',
+    });
+  });
+
+  it('should handle LOGIN', () => {
+    expect(authReducer({}, loginAction)).toEqual({
+      user: user,
+    });
+  });
+
+  it('should handle LOGOUT', () => {
+    expect(authReducer({ user: user }, logoutAction)).toEqual({
+      user: '',
+    });
+  });
+
+});
